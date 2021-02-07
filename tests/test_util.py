f8756381c368f3f50aa6effc40e926aebbe68550
Titanium Hocker <inbox@titaniumhocker.ru>
2021-02-08 01:07:47 +0300

    Updated deps

--- tests/test_util.py
+++ tests/test_util.py
@@ -32,13 +32,3 @@ def test_make_sock(listener):
     assert sock.getsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE) == 1
     assert isinstance(sock, socket.socket)
     # TODO: Find out how to test socket timeout.
-
-
-def test_make_ssh_session(sftpserver):
-    with sftpserver.serve_content({}):
-        assert isinstance(
-            util.make_ssh_session(
-                util.make_socket(sftpserver.host, sftpserver.port)
-            ),
-            Session
-        )
